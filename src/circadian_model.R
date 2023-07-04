72a1a76d31699049dd11312be97dfa3f6c3f1761
Arjun Alva <69946331+arjun149@users.noreply.github.com>
2023-07-04 13:10:33 -0400

    Update circadian_model.R

--- src/circadian_model.R
+++ src/circadian_model.R
@@ -183,7 +183,7 @@ circadianModel <- function(t, x, params){
    for (schoolStartLocalTimeInHours in allSchoolStartOptions) {
     allLux <<- -1 + numeric(numberOfDays * 24 / dt + 1)  # Sanity check vector to make sure we're getting the right lux
     
-    out <- rk4(ic, fullIntegrationWindow, sleep.circ.model, parms)
+    out <- rk4(x, fullIntegrationWindow, sleep.circ.model, params)
     
     
     homeostatLastWeek <- tail(out[,4], 24 * 7 / dt)
