05faf79ada0078eea2e9d1f4ab85c6ab49be5b57
Arjun Alva <69946331+arjun149@users.noreply.github.com>
2023-07-08 13:37:35 -0400

    Update test_model.R

--- test/test_model.R
+++ test/test_model.R
@@ -1,6 +1,6 @@
 # Try importing the circadian_model.R file and running a circadian simulation here
 
-source(file.path("circadianRhythm.R"), local = TRUE)$value
+source(file.path("circadian_Rhythm.R"), local = TRUE)$value
 
 #circadianRhythm(9,default_initial_conditions,params)
 
